ea8971061359f0d0a1a48c66dc513feda779cda9
wixetech <156924108+wixetech@users.noreply.github.com>
2025-03-29 13:42:23 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace SnapGameSaveManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBoxHat = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lastStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBoxPreOrderHat = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(583, 380);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Variables";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.comboBoxColor);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.comboBoxHat);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(571, 352);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cosmetics";
+            // 
+            // comboBoxHat
+            // 
+            this.comboBoxHat.FormattingEnabled = true;
+            this.comboBoxHat.Items.AddRange(new object[] {
+            "None",
+            "Duck",
+            "Apple Cap",
+            "Black Special",
+            "Pre-Order Hat"});
+            this.comboBoxHat.Location = new System.Drawing.Point(341, 19);
+            this.comboBoxHat.Name = "comboBoxHat";
+            this.comboBoxHat.Size = new System.Drawing.Size(219, 21);
+            this.comboBoxHat.TabIndex = 1;
+            this.comboBoxHat.Text = "None";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(11, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(551, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hat: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . " +
+    ". . . . . . . . . . . . . . . . . . . . . . . . .";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBoxColor
+            // 
+            this.comboBoxColor.FormattingEnabled = true;
+            this.comboBoxColor.Items.AddRange(new object[] {
+            "Grey",
+            "Red",
+            "Yellow",
+            "Blue",
+            "Black",
+            "Green",
+            "Dark Blue (its green for some reason)"});
+            this.comboBoxColor.Location = new System.Drawing.Point(341, 43);
+            this.comboBoxColor.Name = "comboBoxColor";
+            this.comboBoxColor.Size = new System.Drawing.Size(219, 21);
+            this.comboBoxColor.TabIndex = 3;
+            this.comboBoxColor.Text = "Grey";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(11, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(551, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Color: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . " +
+    ". . . . . . . . . . . . . . . . . . . . . . . . . .";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox4);
+            this.groupBox3.Controls.Add(this.checkBoxPreOrderHat);
+            this.groupBox3.Location = new System.Drawing.Point(9, 70);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(556, 273);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Promocode Items";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lastStatusLabel,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 445);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(607, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lastStatusLabel
+            // 
+            this.lastStatusLabel.Name = "lastStatusLabel";
+            this.lastStatusLabel.Size = new System.Drawing.Size(60, 17);
+            this.lastStatusLabel.Text = "Nothing...";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(277, 17);
+            this.toolStripStatusLabel2.Text = "| Made by NiGex | Compatible with Snap Game v1.0";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(405, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(210, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Defaults";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBoxPreOrderHat
+            // 
+            this.checkBoxPreOrderHat.AutoSize = true;
+            this.checkBoxPreOrderHat.Location = new System.Drawing.Point(10, 22);
+            this.checkBoxPreOrderHat.Name = "checkBoxPreOrderHat";
+            this.checkBoxPreOrderHat.Size = new System.Drawing.Size(91, 17);
+            this.checkBoxPreOrderHat.TabIndex = 0;
+            this.checkBoxPreOrderHat.Text = "Pre-Order Hat";
+            this.checkBoxPreOrderHat.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(15, 402);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Location = new System.Drawing.Point(8, 133);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(542, 132);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "IMPORTANT INFO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(537, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(530, 70);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "MisterBlack56PlusHat - Black color and black hat.\r\nRoom24_ - Green color\r\nAufrege" +
+    "ndyo25HotDogRacing - Dark blue color";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 467);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snap Game Save Manager";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBoxHat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lastStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBoxPreOrderHat;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+// This shit code was writen in 15 minutes.
+//                                      - NiGex
+
+namespace SnapGameSaveManager
+{
+    public partial class MainForm : Form
+    {
+        private Dictionary<string, string> hatComboBoxMap = new Dictionary<string, string>()
+        {
+            { "Duck\0", "Duck" },
+            { "BlackSpecial\0", "Black Special" },
+            { "PreOrderHat\0", "Pre-Order Hat" },
+            { "AppleCap\0", "Apple Cap" }
+        };
+
+        private Dictionary<string, string> colorComboBoxMap = new Dictionary<string, string>()
+        {
+            { "Grey\0", "Grey" },
+            { "Red\0", "Red" },
+            { "Yellow\0", "Yellow" },
+            { "Blue\0", "Blue" },
+            { "Black\0", "Black" },
+            { "Green\0", "Green" },
+            { "Dark Blue\0", "Dark Blue (its green for some reason)" }
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (RegistryKey regKey = Registry.CurrentUser.OpenSubKey(SNAP_GAME_SAVE_PATH))
+                {
+                    object currentHat = regKey.GetValue("Hat_h193458456", new byte[1] { 0 });
+                    object currentColor = regKey.GetValue("Color_h212387320", new byte[1] { 0 });
+                    object preOrder = regKey.GetValue("PreOrder_h2656643596", 0);
+
+                    if (preOrder.GetType() == typeof(int))
+                        checkBoxPreOrderHat.Checked = (int)preOrder > 0;
+
+                    if (currentColor.GetType() == typeof(byte[]))
+                    {
+                        string color = Encoding.UTF8.GetString((byte[])currentColor).Trim();
+
+                        if (colorComboBoxMap.ContainsKey(color))
+                            comboBoxColor.Text = colorComboBoxMap[color];
+                        else
+                            comboBoxColor.Text = "Grey";
+                    }
+
+                    if (currentHat.GetType() == typeof(byte[]))
+                    {
+                        string hat = Encoding.UTF8.GetString((byte[])currentHat).Trim();
+
+                        if (hatComboBoxMap.ContainsKey(hat))
+                            comboBoxHat.Text = hatComboBoxMap[hat];
+                        else
+                            comboBoxHat.Text = "None";
+                    }
+                }
+
+                MessageBox.Show("Snap Game save was loaded successfully!",
+                    "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lastStatusLabel.Text = "Save was loaded!";
+            }
+            catch
+            {
+                MessageBox.Show("Failed to load Snap Game save. Did you launch it atleast once?",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lastStatusLabel.Text = "Failed to load save!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            checkBoxPreOrderHat.Checked = false;
+            comboBoxColor.Text = "Grey";
+            comboBoxHat.Text = "None";
+            lastStatusLabel.Text = "Default values applied!";
+        }
+
+        private byte[] generateSaveString(string str)
+        {
+            byte[] strBytes = Encoding.UTF8.GetBytes(str);
+            byte[] bytes = { };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(ms))
+                {
+                    writer.Write(strBytes);
+
+                    ms.Position = 0;
+                    using (BinaryReader reader = new BinaryReader(ms))
+                        bytes = reader.ReadBytes((int)ms.Length);
+                }
+            }
+
+            return bytes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (RegistryKey regKey = Registry.CurrentUser.CreateSubKey(SNAP_GAME_SAVE_PATH))
+                {
+                    string hat = "None";
+                    string color = "Grey";
+
+                    foreach (string key in colorComboBoxMap.Keys)
+                    {
+                        if (colorComboBoxMap[key] == comboBoxColor.Text)
+                            color = key;
+                    }
+
+                    foreach (string key in hatComboBoxMap.Keys)
+                    {
+                        if (hatComboBoxMap[key] == comboBoxHat.Text)
+                            hat = key;
+                    }
+
+                    regKey.SetValue("Hat_h193458456", generateSaveString(hat));
+                    regKey.SetValue("Color_h212387320", generateSaveString(color));
+                    regKey.SetValue("PreOrder_h2656643596", checkBoxPreOrderHat.Checked ? 1 : 0);
+                }
+
+                MessageBox.Show("Snap Game save was applied successfully!",
+                    "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lastStatusLabel.Text = "Save was applied successfully!";
+            }
+            catch
+            {
+                MessageBox.Show("Failed to apply Snap Game save.",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lastStatusLabel.Text = "Failed to apply save!";
+            }
+        }
+
+        private const string SNAP_GAME_SAVE_PATH = @"Software\IQ GAMING STUDIO\Snap Game";
+    }
+}
